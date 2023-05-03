d1a2027f1d8d3a67be5ab47d501e54b405b6ff64
masakiShito <98877813+masakiShito@users.noreply.github.com>
2023-05-03 18:29:17 +0900

    プロジェクト作成
    プロジェクト作成　初期コミット

---
+++ src/test/java/com/spring/study/springstudy/SpringstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.study.springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
